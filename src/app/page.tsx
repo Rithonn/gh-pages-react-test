1671736df09144df5ead04e5c80a98f46c8ff2ca
Rithonn <rithonn21@gmail.com>
2025-07-22 17:10:47 -0400

    out?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/out/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -34,7 +34,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/nextjs-github-pages/vercel.svg"
+              src="/out/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -60,7 +60,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/nextjs-github-pages/file.svg"
+            src="/out/file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -75,7 +75,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/nextjs-github-pages/window.svg"
+            src="/out/window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -90,7 +90,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/nextjs-github-pages/globe.svg"
+            src="/out/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
